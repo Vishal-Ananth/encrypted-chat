a98b9c5cc65741ff2189d4ba0f9e06399f4b6b94
Vishal Ananth J <imvishal2002@gmail.com>
2023-11-06 09:43:08 +0530

    bug: restricts connections to 2

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const connections = {};
+
+io.on('connection', (socket) => {
+  if (Object.keys(connections).length >= 2) {
+    socket.emit('connectionLimit');
+    socket.disconnect(true);
+    return;
+  }
+
+  socket.on('encrypt', (data) => {
+    if (Object.keys(connections).length === 2) {
+      for (const key in connections) {
+        if (connections[key] !== socket) {
+          const encryptedData = encryptData(data);
+          connections[key].emit('decrypt', encryptedData);
+        }
+      }
+    }
+  });
+
+  connections[socket.id] = socket;
+
+  socket.on('disconnect', () => {
+    delete connections[socket.id];
+  });
+});
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
